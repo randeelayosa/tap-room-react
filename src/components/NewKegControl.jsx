3bdb24c44b4f0e0f5bd9c4544e535b3bc0c52ccc
randeelayosa <randeelayosa@gmail.com>
2019-04-26 20:03:18 -0700

    Add component for showing and hiding the keg form or beer list.

---
+++ src/components/NewKegControl.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import PropTypes from 'prop-types';
+import BeerList from './BeerList';
+
+class NewKegControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewKegForm onNewKegCreation={this.props.onNewKegCreation}/>;
+    } else {
+      currentlyVisibleContent = <BeerList/>
+    }
+    return(
+      <div>
+        {currentlyVisibleContent}
+      </div>
+    );
+  }
+}
+
+NewKegControl.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegControl;
